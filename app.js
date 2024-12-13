54dc17c3c94fd92c081cca347c86ee78da8bb325
filipeabreujlle <filipeabreujlle@gmail.com>
2024-12-12 22:15:37 -0300

    checkbox adicionadas

--- app.js
+++ app.js
@@ -13,8 +13,24 @@ addTaskBtn.addEventListener("click", () => {
         const li = document.createElement("li");
         li.textContent = task;
 
+        //cria a caixinha (checkbox)
+        const checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+
+        //texto da tarefa para controlar separadamente da caixinha
+        const taskText = document.createTextNode(" " + task); //espaço pra separar a caixinha da task
+
+        //adicionar caixinha e texto dentro do item da lista
+        li.appendChild(checkbox);
+        li.appendChild(taskText);
+
         //vai adicionar a tarefa criada á lista
         taskList.appendChild(li);
+
+        //evento de marcar caixinha e desmarcar
+        checkbox.addEventListener("change", () => {
+            //aplicar style de riscar as tasks aqui
+        })
     }else{
         alert("Digite uma tarefa válida!");
     }
