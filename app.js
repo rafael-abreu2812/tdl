dd970371a4b7af1f35fc6b55a5fda1115edfc2a1
filipeabreujlle <filipeabreujlle@gmail.com>
2024-12-13 23:10:06 -0300

    centralizacao e remocao botao

--- app.js
+++ app.js
@@ -1,37 +1,43 @@
-//Botão e lista do html
-const addTaskBtn = document.getElementById("addTaskBtn");
+//Lista do html e campo de entrada
+const taskInput = document.getElementById("taskInput");
 const taskList = document.getElementById("taskList");
 
 //Evento de clique do botão
-addTaskBtn.addEventListener("click", () => {
-    //prompt pedindo a tarefa
-    const task = prompt("Digite aqui a tarefa!");
+taskInput.addEventListener("keydown", (event) => {
 
-    //verificando se algo foi digitado
-    if(task && task.trim() !== ""){
-        //vai criar uma nova tarefa na lista
-        const li = document.createElement("li");
-        
-        //cria a caixinha (checkbox)
-        const checkbox = document.createElement("input");
-        checkbox.type = "checkbox";
+    if (event.key === "Enter") {
 
-        //texto da tarefa para controlar separadamente da caixinha
-        const taskText = document.createTextNode(" " + task); //espaço pra separar a caixinha da task
+        //pegar o valor digitado no campo de entrada
+        const task = taskInput.value;
 
-        //adicionar caixinha e texto dentro do item da lista
-        li.appendChild(checkbox);
-        li.appendChild(taskText);
+        //verificando se algo foi digitado
+        if (task && task.trim() !== "") {
+            //vai criar uma nova tarefa na lista
+            const li = document.createElement("li");
 
-        //vai adicionar a tarefa criada á lista
-        taskList.appendChild(li);
+            //cria a caixinha (checkbox)
+            const checkbox = document.createElement("input");
+            checkbox.type = "checkbox";
 
-        //evento de marcar caixinha e desmarcar
-        checkbox.addEventListener("change", () => {
-            //aplicar style de riscar as tasks aqui
-        })
-    }else{
-        alert("Digite uma tarefa válida!");
+            //texto da tarefa para controlar separadamente da caixinha
+            const taskText = document.createTextNode(" " + task); //espaço pra separar a caixinha da task
+
+            //adicionar caixinha e texto dentro do item da lista
+            li.appendChild(checkbox);
+            li.appendChild(taskText);
+
+            //vai adicionar a tarefa criada á lista
+            taskList.appendChild(li);
+
+            //limpar o campo de entrada após adicionar uma tarefa
+            taskInput.value = "";
+
+            //evento de marcar caixinha e desmarcar
+            checkbox.addEventListener("change", () => {
+                //aplicar style de riscar as tasks aqui
+            })
+        } else {
+            alert("Digite uma tarefa válida!");
+        }
     }
-
 });
