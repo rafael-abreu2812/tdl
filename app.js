23dc3bd01a1a6f78572533f59f0a39d7eb75534e
rafael-abreu2812 <rafael.abreu2812@gmail.com>
2025-01-09 21:25:57 -0300

    add css na lista de tarefas

--- app.js
+++ app.js
@@ -18,13 +18,21 @@ taskInput.addEventListener("keydown", (event) => {
             //cria a caixinha (checkbox)
             const checkbox = document.createElement("input");
             checkbox.type = "checkbox";
+            checkbox.id = `task-${Date.now()}`;
+
+
+            const label = document.createElement("label");
+            label.setAttribute("for", checkbox.id);
+            label.textContent = task;
+        
 
             //texto da tarefa para controlar separadamente da caixinha
             const taskText = document.createTextNode(" " + task); //espaço pra separar a caixinha da task
 
             //adicionar caixinha e texto dentro do item da lista
             li.appendChild(checkbox);
-            li.appendChild(taskText);
+            li.appendChild(label);
+            
 
             //vai adicionar a tarefa criada á lista
             taskList.appendChild(li);
